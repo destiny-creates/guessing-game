e0c5cd61198f5f77fb090d3fd2373bc5ec48056e
destiny creates <dronesforaliving@gmail.com>
2018-07-17 13:42:55 -0400

    added code to generate random number

--- Guessing Game/ViewController.swift
+++ Guessing Game/ViewController.swift
@@ -14,21 +14,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var guessingField: UITextField!
     @IBOutlet weak var submitButton: UIButton!
     @IBOutlet weak var resetButton: UIButton!
+    var randomNumber: Int!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-gameStatusLabel.text = "we've changed the text here"
-        submitButton.setTitle("Submit", for: .normal)
-resetButton.setTitle("Reset", for: .normal)
-
+randomNumber = Int(arc4random_uniform(101))
+        gameStatusLabel.text = "\(randomNumber)"
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func submitButtonTapped(_ sender: Any) {
+        
+        gameStatusLabel.text = ("you tapped the submit button")
+}
+    @IBAction func ResetButton(_ sender: Any) {
+        gameStatusLabel.text = "you tapped reset button"
+    }
+    
+
+
 
 }
+    
+
 
