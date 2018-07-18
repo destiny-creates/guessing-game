e80db550e6f16299906fdd8f329dd4f45251ea23
destiny creates <dronesforaliving@gmail.com>
2018-07-18 14:46:33 -0400

    finished logic

--- Guessing Game/ViewController.swift
+++ Guessing Game/ViewController.swift
@@ -9,49 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var gameStatusLabel: UILabel!
     @IBOutlet weak var guessingField: UITextField!
     @IBOutlet weak var submitButton: UIButton!
     @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var attemptsRemainingLabel: UILabel!
+    
     var randomNumber: Int!
+    var attempts = 5
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-randomNumber = Int(arc4random_uniform(101))
+        randomNumber = Int(arc4random_uniform(101))
+        attemptsRemainingLabel.text = "5"
         gameStatusLabel.text = "good luck!"
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func submitButtonTapped(_ sender: Any) {
         guard let userGuess = Int(guessingField.text!) else {
             return
         }
+        
         if userGuess < 0 || userGuess > 100 {
             return
         }
-        if userGuess == randomNumber {
-            gameStatusLabel.text = "correct! you won!"
-        } else if userGuess < randomNumber {
-            gameStatusLabel.text = "your guess was to low"
+        
+        attempts -= 1
+        attemptsRemainingLabel.text = "\(attempts)"
+        
+        if attempts > 0 {
+            
+            if userGuess == randomNumber {
+                gameStatusLabel.text = "correct! you won!"
+            } else if userGuess < randomNumber {
+                gameStatusLabel.text = "your guess was to low"
+            } else {
+                gameStatusLabel.text = "your guess was to high"
+            }
+        } else {
+            gameStatusLabel.text = ("you lose! correct number was \(randomNumber!). try again!")
+            submitButton.isEnabled = false
         }
-        else {
-            gameStatusLabel.text = "your guess was to high"
-        }
-    
-    }
-    @IBAction func ResetButton(_ sender: Any) {
-        gameStatusLabel.text = ("you tapped reset button")
+        
     }
     
-
-
-
+    @IBAction func resetButtonTapped(_ sender: Any) {
+        gameStatusLabel.text = "Good luck!"
+        attemptsRemainingLabel.text = "5"
+        randomNumber = Int(arc4random_uniform(101))
+        submitButton.isEnabled = true
+        attempts = 5
+    }
 }
-    
+
+
+
 
 
